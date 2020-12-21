d87d9990a3b10f72e1a0dbcb8072fa73ff8b82ec
swasthikNitturi <swasthik305@gmail.com>
2020-12-21 16:07:19 +0530

    calculator

---
+++ Calculator/src/main/java/Calculator.java
@@ -0,0 +1,14 @@
+
+public class Calculator {
+
+	public static int add(int a,int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Sytem.out.println("sasd");
+		System.out.println(add(10,20));
+
+	}
+
+}
